8149abae1b6290afda831e9cdeae20349e484cf1
Mara <baudenam@users.noreply.github.com>
2019-09-28 14:25:22 +0200

    Add files via upload

---
+++ ShortTermExp/mainfire_shortterm.f90
@@ -0,0 +1,337 @@
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+!                      PROGRAM TO MODEL FIRES 
+!                IN THE MEDITERRANEAN FORESTS UNDER INCREASED ARIDITY  !
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+! THIS CODE HAS BEEN DEVELOPED WITHIN THE PAPER BAUDENA ET AL 2019
+! NEW PHYTOLOGIST (OCT 1ST 2019: ACCEPTED SUBJECT TO MINOR REVISIONS)
+
+!  THE MODEL INCLUDES 6 VEGETATION TYPES (IN ORDER OF COMPETITION)
+! 1- QUERCUS: RESPROUTER
+! 2- P. HALEPENSIS: SEEDER
+! 3- ROSEMARY     |
+! 4- ULEX         | SHRUB SEEDERS
+! 5- CISTUS       |
+! 6- BRACHIPODIUM: GRASS RESPROUTER
+
+! THIS VERSION IS USED TO SIMULATE SHORTER TIMESCALES,
+! RELEVANT IN A HUMAN, CLIMATE CHANGE CONTEST.
+!
+!   1- RUN LENGTH: 100Y
+!   2- # OF DIFFERENT FIRE STOCHASTIC SEQUENCES (RANDOM SEEDS): 100
+!   3- THREE INITIAL CONDITION USED: FOREST (i.c. 2) AND SHRUBLAND/GRASSLAND (i.c. 6) (NB numbers refer to PARAFIRE file) + SUCCESSIONAL MIXTURE (i.c. 8).
+!   4- FINAL AVERAGE CALCULATED OVER LAST 20 YEARS
+
+program mainfire_shortterm
+use parafire
+
+implicit none
+
+double precision,dimension(6) ::  b,db,f,df,fout,bav,bavf,alpha, Sbank
+double precision  :: numd
+
+integer           ::  i,NN,ii
+
+! FOR FDB FILE:
+real            ::  firevf=100000,deltaff=100000
+integer         ::  iifire=0, idummy!=627
+integer         ::  num, numok
+real            ::  dummy, ran3
+
+! for loop file:
+integer             :: iflamm,initcond, ifrac, id, icol
+character (len=1)   :: iflstr,initstr, icolstr
+character (len=2)   :: ifracstr
+character (len=3)   :: idummystr
+character (len=21)  :: filename
+
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+! CODE STARTS
+
+NN=dimfv  ! NUMBER OF INTEGRATION TIMESTEP (SAME AS dimfv BECAUSE h=1)
+
+
+open (22,file=filelfdb)
+
+
+do id=100,199               ! stochastic fire loop
+do iflamm=1,4               ! increasing flammabilities UP TO x3
+do initcond=9,9             ! loop on init cond
+do ifrac=60,90,15           ! loop on fracR values =ifrac/100 CHANGED R1
+do icol=1,4                 ! loop on c1 values
+    write(idummystr,'(I3)') id
+    write(iflstr,'(I1)') iflamm
+    write(initstr,'(I1)') initcond
+    write(ifracstr,'(I2)') ifrac
+    write(icolstr,'(I1)') icol
+
+    filename='bTime'//idummystr//'_'//iflstr//'_'//initstr//'_'//ifracstr//'_'//icolstr//'.dat'
+    open (21,file=filename)
+
+    !     params initialization
+    firevf=100000
+    deltaff=100000
+    iifire=0
+    fracR(1)=ifrac/100.
+    idummy=id
+    l=l0*(1+flammincr(iflamm))
+    c1=0.047-(icol-1.)*0.012
+
+
+    !     variables initialization
+    b=b0ic(:,initcond)
+    bav=0.*b
+    bavf=b0ic(:,initcond)*deltaff/100. ! THIS IS B SUMMED UP BETWEEN FIRES, AND AVERAGED AT FIRES (CALCULATED TO BE USED FOR SEEDERS)
+
+    ! intialize alpha (no fire)
+    alpha=0.!alp0
+    Sbank=Sbank0
+    !  IN PROPORTION TO B0 FOR SHRUB SEEDERS
+    Sbank(3:5)=seedpropb*b0ic(3:5,initcond)
+    ! seed bank initialisation
+    call alphacalc(firevf,deltaff,iifire,bavf,b,Sbank,alpha)
+
+
+    !     temporal loop
+    do i=1,(4*NN/5)
+          
+          ! integration continous dynamics
+          f=b 
+          call derivs(i,f,df, alpha)
+          call rk4(i,f,df,fout,alpha)
+          f=fout
+          ! stochastic fire dynamics (with fdb)
+          dummy=ran3(idummy)
+
+          numd=1/(l(1)*b(1)+l(2)*b(2)+l(3)*b(3)+l(4)*b(4)+l(5)*b(5)+l(6)*b(6)+eps)   ! years (from about 2 to 500 y return time)
+          numok=nint(numd)
+          num=nint(dummy*365*numok)              ! days
+          if (num.eq.numok*365 .and. firevf>minfirerettime*365.) then
+            iifire=1                            ! fire!
+            deltaff=firevf
+            firevf=0.
+            bavf=bavf/deltaff*100. ! it is used by alphacalc and then reset to zero there
+          else
+            iifire=0
+            firevf=firevf+h
+            bavf=bavf+b/100.
+          end if
+
+          ! vegetation burns:
+          call fireocc(f,iifire,fout)
+          b=fout
+          call alphacalc(firevf,deltaff,iifire,bavf,b,Sbank,alpha)
+
+          if (mod(i,100).eq.0 .or. iifire>0) then
+            write (21,'(1i15,6f15.4,1i15)') i,b,iifire
+          end if
+    end do
+
+    !go to 1000
+    ii=0
+    do i=(4*NN/5)+1,NN
+        ii=ii+1
+        ! integration continous dynamics
+        f=b
+        call derivs(i,f,df, alpha)
+        call rk4(i,f,df,fout,alpha)
+        f=fout
+
+        ! stochastic fire dynamics (with fdb)
+        dummy=ran3(idummy)
+        numd=1/(l(1)*b(1)+l(2)*b(2)+l(3)*b(3)+l(4)*b(4)+l(5)*b(5)+l(6)*b(6)+eps)   ! years (from about 2 to 500 y return time)
+        numok=nint(numd)
+        num=nint(dummy*365*numok)              ! days
+        if (num.eq.numok*365 .and. firevf>minfirerettime*365.) then
+            iifire=1                            ! fire!
+            deltaff=firevf
+            firevf=0.
+           bavf=bavf/deltaff*100. ! it is used by alphacalc and then reset to zero there
+        else
+            iifire=0
+            firevf=firevf+h
+            bavf=bavf+b/100.
+        end if
+
+        ! vegetation burns:
+        call fireocc(f,iifire,fout)
+        b=fout
+        call alphacalc(firevf,deltaff,iifire,bavf,b,Sbank,alpha)
+        if (mod(i,100).eq.0 .or. iifire>0) then
+            write (21,'(1i15,6f15.4,1i15)') i,b,iifire
+        end if
+
+        bav=bav+b/1000
+    end do
+    bav=bav/ii*1000
+    write (* ,'(1i15,6f10.4,5i8,6f10.4)') i,bav, id, iflamm, initcond,ifrac,icol,l
+    write (22 ,'(1i15,6f10.4,5i8,6f10.4)') i,bav, id, iflamm, initcond,ifrac,icol,l
+end do
+end do
+end do
+end do
+end do
+
+end program mainfire_shortterm
+
+
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+
+        subroutine rk4(tt,y,dydx,yout,g) ! RUNGE-KUPTA INTEGRATION ROUTINE
+        use parafire
+        
+        implicit none
+        
+        real                          :: h6
+        double precision,dimension(6) :: dydx,y,yout,k1,k2,k3,k4,y1,y2,y3, g(6)
+        integer                       :: tt
+        h6=hy/6.
+
+        k1=dydx
+        y1=y+0.5*k1*hy
+        call derivs(tt,y1,k2,g)
+        y2=y+0.5*k2*hy
+        call derivs(tt,y2,k3,g)
+        y3=y+k3*hy
+        call derivs(tt,y3,k4,g)
+
+        yout=y+h6*(k1+2*k2+2*k3+k4)
+
+        return
+        end
+
+
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+
+        subroutine derivs(tt,v,dv,a) ! RIGHT HAND SIDE OF ODEs
+!       v vegetation cover, dv derivatives
+        use parafire
+        
+        implicit none
+        integer               :: tt
+        double precision,dimension(6)  :: v,dv
+        double precision :: a(6), aa(6) ! ALPHA IS ZERO ALWAYS FOR RESPROUTERS (1&6)
+
+        aa=C*a/(a(2)+a(3)+a(4)+a(5)+.0001)*(1-v(1)-v(2)-v(3)-v(4)-v(5)-v(6))
+        
+        dv(1)=c1*v(1)*(1-v(1))-m1*v(1)
+        dv(2)=(c2*v(2))*(1-v(1)-v(2))-m2*v(2)-c1*v(1)*v(2)+aa(2)
+        dv(3)=(c3*v(3))*(1-v(1)-v(2)-v(3))-m3*v(3)-c2*v(2)*v(3)-c1*v(1)*v(3)+aa(3)
+        dv(4)=(c4*v(4))*(1-v(1)-v(2)-v(3)-v(4))-m4*v(4)-(c2*v(2)+c1*v(1)+c3*v(3))*v(4)+aa(4)
+        dv(5)= (c5*v(5))*(1-v(1)-v(2)-v(3)-v(4)-v(5))- m5*v(5) -(c2*v(2)+c1*v(1)+c3*v(3)+c4*v(4))*v(5)+ aa(5)
+        dv(6)= c6*v(6)*(1-v(1)-v(2)-v(3)-v(4)-v(5)-v(6)) - m6*v(6)-(c2*v(2)+c1*v(1)+c3*v(3)+c4*v(4)+c5*v(5))*v(6)
+
+        return
+        end
+        
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+
+        subroutine alphacalc(tl,dtl,iifire,vavf,v,sb,a)
+        use parafire
+        
+        implicit none
+        integer :: iifire ! 1-THERE IS A FIRE TODAY, 0 OTHERW.
+        real :: tl,dtl, tpine ! tl=t last fire, dtl= t between last two fires
+        double precision :: fsa, fsl
+        double precision,dimension(6) :: vavf,v,a, sb
+
+        ! PINES
+        ! FUNCTION OF THE TIME FROM LAST FIRE tl: GIVES A PEAK FOR TWO YEARS AND THEN ZERO
+        fsa=1/2.-1/2.*tanh((tl/365.-pseed+amp)/amp)
+        ! FUNCTION THAT DETERMINES WHETHER THERE WERE MATURE PINES BEFORE LAST FIRE  
+        fsl=1/2.*(1+tanh((dtl/365.-pmat)/amp2))
+
+!       TO ASSURE THIS TERM DOESN'T GET NEGATIVE: SB2 IS PROPORTIONAL TO THE TIME BETWEEN LAST 2 FIRES -MINUS- THE MATURITY AGE OF PINE
+        tpine=(dtl/365-pmat)*fsl
+        sb(2)=iifire*(vavf(2)*sp(2)*aseed*tpine*fsl)+sb(2)*fsa*(1-iifire)
+        
+!        ! OTHER SEEDERS: THE SEED BANK IS CALCULATED EACH STEP
+!        BETWEEN FIRES IT DECREASES ONLY WITH TIME FROM FIRE
+!       AT FIRES IT STEEPLY DECRESES IN PROPORTION TO SURVIVAL RATE AND IT IS INCREASED IN PROPORTION TO THE AVERAGE COVER BEFORE FIRE
+!
+        SbankF(3)=iifire*ss(3)*(sb(3)+vavf(3)*sp(3)*dtl/365)+(1-iifire)*SbankF(3)
+        SbankF(4)=iifire*ss(4)*(sb(4)+vavf(4)*sp(4)*dtl/365)+(1-iifire)*SbankF(4)
+        SbankF(5)=iifire*ss(5)*(sb(5)+vavf(5)*sp(5)*dtl/365)+(1-iifire)*SbankF(5)
+
+        sb(3)=SbankF(3)*exp(-3*tl/365*ms(3))
+        sb(4)=SbankF(4)*exp(-3*tl/365*ms(4))
+        sb(5)=SbankF(5)*exp(-3*tl/365*ms(5))
+
+        ! ALPHA IS CALCULATED FROM THE SEED BANKS KEEPING INTO ACCOUNT
+        ! GERMINATION RATE AND SEEDLING SURVIVAL
+        ! NB C is not needed here because they are renormalised to the sum of alphas in the derivs subroutine
+        
+        a(2)=e(2)*sb(2)*gp(2)*sgp(2)
+        a(3)=e(3)*sb(3)*gp(3)*sgp(3)
+        a(4)=e(4)*sb(4)*gp(4)*sgp(4)
+        a(5)=e(5)*sb(5)*gp(5)*sgp(5)
+
+        ! RESET vavf in case of fire
+        vavf=vavf*(1-iifire)+0.*iifire
+        
+        return
+        end
+
+        
+        
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+
+      subroutine fireocc(b,iifire,bout)
+      use parafire
+      
+      implicit none
+      
+      double precision,dimension(6)       :: b,bout!,bbef
+      integer                             :: iifire
+
+
+      bout=b
+      bout(1)=b(1)*(1-iifire+fracR(1)*iifire) ! RESPROUTER
+      bout(2)=b(2)*(1-iifire)!+eps*iifire   ! SEEDER eps part is needed only without alphas
+      bout(3)=b(3)*(1-iifire)!+eps*iifire   ! SEEDER
+      bout(4)=b(4)*(1-iifire)!+eps*iifire   ! SEEDER
+      bout(5)=b(5)*(1-iifire)!+eps*iifire   ! SEEDER      
+      bout(6)=b(6)*(1-iifire+fracR(6)*iifire) ! RESPROUTER
+      
+     
+      end subroutine
+
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+
+      FUNCTION RAN3(IDUM) ! NUMERICAL RECIPES
+      SAVE
+      PARAMETER (MBIG=1000000000,MSEED=161803398,MZ=0,FAC=1.E-9)
+      DIMENSION MA(55)
+      DATA IFF /0/
+      IF(IDUM.LT.0.OR.IFF.EQ.0)THEN
+        IFF=1
+        MJ=MSEED-IABS(IDUM)
+        MJ=MOD(MJ,MBIG)
+        MA(55)=MJ
+        MK=1
+        DO 11 I=1,54
+          II=MOD(21*I,55)
+          MA(II)=MK
+          MK=MJ-MK
+          IF(MK.LT.MZ)MK=MK+MBIG
+          MJ=MA(II)
+11      CONTINUE
+        DO 13 K=1,4
+          DO 12 I=1,55
+            MA(I)=MA(I)-MA(1+MOD(I+30,55))
+            IF(MA(I).LT.MZ)MA(I)=MA(I)+MBIG
+12        CONTINUE
+13      CONTINUE
+        INEXT=0
+        INEXTP=31
+        IDUM=1
+      ENDIF
+      INEXT=INEXT+1
+      IF(INEXT.EQ.56)INEXT=1
+      INEXTP=INEXTP+1
+      IF(INEXTP.EQ.56)INEXTP=1
+      MJ=MA(INEXT)-MA(INEXTP)
+      IF(MJ.LT.MZ)MJ=MJ+MBIG
+      MA(INEXT)=MJ
+      RAN3=MJ*FAC
+      RETURN
+      END
---
+++ ShortTermExp/parafire.f90
@@ -0,0 +1,97 @@
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+! parameters for "mediterrenanean fires"                                            !
+!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++!
+
+module parafire
+
+implicit none
+
+! COLONIZATION AND MORTALITY RATES
+real :: c1  = 0.047       ! y^-1
+real :: m1  = 1/400.      ! y^-1 0.0025
+real :: c2  = 0.053        ! y^-1
+real :: m2  = 1/125.      ! y^-1 0.008
+real :: c3  = .045         ! y^-1
+real :: m3  = 1/50.       ! y^-1 0.02
+real :: c4  = .067         ! y^-1
+real :: m4  = 1/25.       ! y^-1 0.04
+real :: c5  = .11         ! y^-1
+real :: m5  = 1/15.       ! y^-1 0.066667
+real :: c6  = .22         ! y^-1
+real :: m6  = 1/40.       ! y^-1 0.025
+
+
+double precision  :: b0ic(6,9) =reshape((/0.01, 0.01, 0.01, 0.01, 0.01, 0.01, & ! low all
+0.89, 0.01, 0.01, 0.01, 0.01, 0.01, &  !  high Q
+0.01, 0.89, 0.01,0.01, 0.01, 0.01, &  !  high P
+0.01, 0.1, 0.59, 0.1, 0.1, 0.1,     & ! high R+other shrubs
+0.01, 0.1, 0.1, 0.59, 0.1, 0.1,    &  ! high U+other shrubs
+0.01,0.1,0.1,0.1, 0.59, 0.1,       &  ! high C+other shrubs
+0.01, 0.1, 0.1, 0.1, 0.1,0.59, &  !   high B+shrubs
+0.15, 0.15, 0.15, 0.15, 0.15, 0.15, & ! successional mixture
+0.01, 0.01, 0.15, 0.15, 0.15, 0.15/),(/6,9/)) ! shrubs +B
+
+real  :: Sbank0(6) = (/0., 0.,50., 50.,50.,0. /)
+real  :: SbankF(6) = 0.
+double precision  :: seedpropb=50. !Proportion of seed to cover
+
+! FILE NAMES
+character (len=12)::  file1='bTime.dat'
+character (len=12)::  filep='prova.dat'
+character (len=9) ::  fileff='fire1.dat'
+character (len=12)::  filealpha='alpha.dat'
+character (len=14)::  filel='bLoopdi.dat'
+character (len=15)::  filelfdb='bLoopFdbdi.dat'
+
+
+! OTHER PARAMS
+integer, parameter :: maxT   = 100              ! y,  Number of years FROM 1ST FIRE when to stop integration (LONGER THAN integration time)
+integer,parameter :: dimfv =maxT*365                    ! DIMENSION OF FIRE VECTOR (ONE A DAY)
+! ALSO: d,  Number of years for integration
+real,parameter   :: h   = 1               ! d, delta t of integration LEAVE IT TO 1 DAY,DO
+real    :: hy=h/365                      ! y, delta t of integration
+!real    :: Tvect(4)=(/5, 20,50,100/)! y, periods of fire occurence
+real    :: Tvect(4)=(/10,20,30,40/)! y, periods of fire occurence
+integer :: ifire  = 2               ! 1 to 4, index of Tvect
+!integer :: Tmoist = 20              ! d, periods of colonization perturbation
+
+!real :: l(6)=(/.002, 0.1, .02, 0.05, .143, .4/) ! y^-1 flammability frequencies (before Ramon) -> I INVENTED THESE NUMBERS IT SEEMS! SO NO USE
+!real :: l(6)=(/.002, 0.05, .0666667, 0.1, .1, .1/) ! y^-1 flammability frequencies (advice Ramon)
+real :: l0(6)=(/.002, 0.05, .0666667, 0.1, .1, .1/) ! y^-1 flammability frequencies (from Ramon's, increased for experiments)
+!real :: l(6)
+!real :: l(6)=(/.002, 0.04, .0666667, 0.1, .1, .2/) ! y^-1 flammability frequencies (advice J&V)
+real :: l(6)=(/.00, 0., .0, 0.0, .0, .0/) ! y^-1 zero flammability frequencies -> no fire
+real :: flammincr(5)=(/.0, .2, .5, 2., 4./)       ! flammability increase because of drought
+
+real    :: eps    = .0001             ! y^-1, minimum fire frequency when feedback
+real    :: fracR(6)  = (/0.,0.,0.,0.,0.,0.4/)             ! fraction of resprouter surviving a fire
+!real    :: M      = 100             ! value of the max seed production 1-2y after fire
+real    :: aseed = 1               ! y^-1; param (a)dimensionalising the Seeds
+!real    :: amax(6)= (/0., 20., .001, .001,.001 ,0./)      ! value of the max seed production (FOR PINES:1-2y after fire)
+real    :: amp    = 0.1             ! tanh width for seed germination
+real    :: amp2   = 4.             ! tanh width for pine maturity
+!real    :: beta   = 25             ! y, shrub seeds half life
+real    :: pmat   = 12              ! y, pine maturity
+real    :: pseed  = 2               ! y, end of pine seed germination stimulation after fire
+real    :: gp(6)  = (/0., .13, .09, .08, .03, 0./) ! %, seed germination percent after fire
+real    :: sgp(6) = (/0., .31, .36, .36, .26, 0./) ! %, seedling survival
+real    :: e(6) = (/0., 1., 0.5, 0.1, .1, 0./) ! % 1/{# seed that would guarantee dominance, pine=1}
+real    :: ms(6) = (/0., .0, .1, .01, .02, 0./)    ! mean seed decay rate (y^-1) from expert estimation of seed durability
+real    :: ss(6) = (/0., 0., .45, .8, .75, 0./)! (/0., 0., .3, .8, .75, 0./)      ! %, seed survival to fire (NB THIS CAN CHANGE  SEE DOC -> VARY THIS PARAM)
+real    :: sp(6) = (/0., 1., 48., 18., 242., 0./)  ! seed production in proportion to pine
+!real    :: C(6) =  (/.0, .01, .01, .01, .01, .01/)       ! conversion rate from seeds to (%space per y-1)
+real    :: C = .014                  ! FROM OPTIMIZATION
+!real    :: Cvec(4) = (/.01, .1, 1., 10./)
+real    :: minfirerettime = 2       ! y
+integer :: idumout ! seed for fire stoch
+
+! FIRE VECTORS
+real,dimension(dimfv)    :: firev ! DAYS FROM LAST FIRE EVENT: 0=FIRE, #=DAYS AFTER FIRES
+real,dimension(dimfv)    :: deltaf ! # DAYS BETWEEN LAST TWO FIRE EVENTS
+integer,dimension(dimfv) :: ifv    !  0=NO FIRE, 1=FIRE
+
+
+! CONSTANTS
+DOUBLE PRECISION, PARAMETER :: pi = 3.141592653589d0
+
+end module parafire
